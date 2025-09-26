b7c051511e7d8bb2db770d151b96e41935c09a9b
Felipe <felipe.stlbus@gmail.com>
2025-09-25 21:04:41 -0300

    feat: adicionando mongodb

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const multer = require('multer');
+const mongoose = require('mongoose');
 
 app.use(cors({
     origin: '*',
@@ -337,6 +339,297 @@ app.post('/init-db', async (req, res) => {
 
 //#endregion
 
+//#region CRUD MongoDb
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB conectado', null))
+    .catch(err => console.log('Erro ao logar mongodb' + err, null, err));
+
+const UserSchema = new mongoose.Schema({
+    nome: String,
+    email: String
+});
+
+const User = mongoose.model('Usuario', UserSchema);
+
+/**
+ * @swagger
+ * /mongodb/testar-conexao:
+ *   get:
+ *     tags:
+ *       - CRUD MongoDb
+ *     summary: Testa a conexão com o MongoDB
+ *     description: Verifica se a aplicação consegue se conectar ao MongoDB.
+ *     responses:
+ *       200:
+ *         description: Conexão bem-sucedida
+ *       500:
+ *         description: Erro na conexão com o MongoDB
+ */
+app.get('/mongodb/testar-conexao', async (req, res) => {
+    try {
+        //Tentando conectar ao MongoDB
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        const user = await User.findOne(); //Consulta simples (primeiro usuário encontrado)
+
+        console.log('Conexão com o MongoDB efetuada com sucesso', req);
+
+        if (user) {
+            res.status(200).send('Conexão com o MongoDB bem-sucedida e usuário encontrado!');
+        } else {
+            res.status(200).send('Conexão com o MongoDB bem-sucedida, mas nenhum usuário encontrado.');
+        }
+    } catch (error) {
+        await console.log('Erro ao conectar no MongoDb' + error, req, error);
+        res.status(500).send('Erro na conexão com o MongoDB');
+    } finally {
+        mongoose.connection.close();
+    }
+});
+
+/**
+ * @swagger
+ * /usuarios:
+ *   post:
+ *     tags:
+ *       - CRUD MongoDb
+ *     summary: Criar um novo usuário
+ *     description: Este endpoint cria um novo usuário no sistema.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do usuário
+ *               email:
+ *                 type: string
+ *                 description: Email do usuário
+ *             required:
+ *               - name
+ *               - email
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: ID do usuário
+ *                 name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       400:
+ *         description: Requisição inválida.
+ */
+app.post('/usuarios', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        console.log('Usuário criado', req);
+        res.status(201).send(user);
+    } catch (error) {
+        console.log("Erro ao criar usuário", req, error);
+        res.status(500).send('Ocorreu um erro interno');
+    }
+});
+
+/**
+ * @swagger
+ * /usuarios:
+ *   get:
+ *     tags:
+ *       - CRUD MongoDb
+ *     summary: Listar todos os usuários
+ *     description: Este endpoint retorna todos os usuários cadastrados no sistema.
+ *     responses:
+ *       200:
+ *         description: Lista de usuários
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   nome:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ */
+app.get('/usuarios', async (req, res) => {
+    try {
+        const users = await User.find();
+        console.log('Usuários encontrados', req, users);
+        res.send(users);
+    } catch (error) {
+        console.log("Erro ao buscar usuários", req, error);
+        res.status(500).send('Ocorreu um erro interno');
+    }
+
+});
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   get:
+ *     tags:
+ *       - CRUD MongoDb
+ *     summary: Obter um usuário específico
+ *     description: Este endpoint retorna um usuário baseado no ID fornecido.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID do usuário
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Usuário encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 nome:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       404:
+ *         description: Usuário não encontrado.
+ */
+app.get('/usuarios/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).send('Usuário não encontrado');
+
+        console.log('Usuário encontrado', req, user);
+        res.send(user);
+    } catch (error) {
+        console.log("Erro ao buscar usuário", req, error);
+        res.status(500).send('Ocorreu um erro interno');
+    }
+
+});
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   put:
+ *     tags:
+ *       - CRUD MongoDb
+ *     summary: Atualizar um usuário específico
+ *     description: Este endpoint atualiza um usuário baseado no ID fornecido.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID do usuário
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 nome:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       404:
+ *         description: Usuário não encontrado.
+ */
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) return res.status(404).send('Usuário não encontrado');
+
+        console.log('Usuário atualizado', req, user);
+        res.send(user);
+    } catch (error) {
+        console.log("Erro ao atualizar usuário", req, error);
+        res.status(500).send('Ocorreu um erro interno');
+    }
+});
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   delete:
+ *     tags:
+ *       - CRUD MongoDb
+ *     summary: Remover um usuário específico
+ *     description: Este endpoint remove um usuário baseado no ID fornecido.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID do usuário
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Usuário removido
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 nome:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       404:
+ *         description: Usuário não encontrado.
+ */
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const result = await User.deleteOne({ _id: req.params.id });
+        if (result.deletedCount === 0) {
+            return res.status(404).send('Usuário não encontrado');
+        }
+
+        console.log('Usuário removido', req);
+        res.send({ message: 'Usuário removido com sucesso' });
+    } catch (error) {
+        console.log("Erro ao remover usuário", req, error)
+        res.status(500).send('Ocorreu um erro interno');
+    }
+
+});
+//#endregion
 
 swaggerDocs(app);
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
